a35b341e606d10ead570218293911d4fdc5f4277
Mennatullah Ayman Fathy <151868435+mennaayman213@users.noreply.github.com>
2025-03-14 14:53:29 +0200

    Update Multithreading.java

--- Multithreading.java
+++ Multithreading.java
@@ -1,28 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package multithreading;
-
 /**
  *
  * @author menna
  */
-//public class Multithreading {
-//
-//    /**
-//     * @param args the command line arguments
-//     */
-//    public static void main(String[] args) {
-//        // TODO code application logic here
-//    }
-//    
-//}
-//package multithreading;
 
+package multithreading;
 public class Multithreading {
     public static void main(String[] args) {
-        // إنشاء 3 ثريدات جديدة
+
         for (int i = 1; i <= 3; i++) {
             Thread thread = new Thread(new Task(i));
             thread.start();
@@ -41,7 +25,7 @@ class Task implements Runnable {
     public void run() {
         System.out.println("🔹 Task " + taskId + " is running on thread: " + Thread.currentThread().getName());
         try {
-            Thread.sleep(2000); // محاكاة عملية طويلة
+            Thread.sleep(2000); 
         } catch (InterruptedException e) {
             System.out.println("Error: " + e.getMessage());
         }
